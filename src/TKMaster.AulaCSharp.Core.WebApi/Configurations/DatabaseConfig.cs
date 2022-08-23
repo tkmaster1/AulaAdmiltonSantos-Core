1d847cf57e332ffeb3004c9610bc1307f03dec9b
tkmaster1 <45917644+tkmaster1@users.noreply.github.com>
2022-08-23 20:55:47 -0300

    Criação do projeto webApi para configuraçaõ da conexão de banco.

---
+++ src/TKMaster.AulaCSharp.Core.WebApi/Configurations/DatabaseConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TKMaster.AulaCSharp.Core.Data.Context;
+
+namespace TKMaster.AulaCSharp.Core.WebApi.Configurations
+{
+    public static class DatabaseConfig
+    {
+        public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddDbContext<MeuContextoDB>(
+                options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                );
+        }
+    }
+}
---
+++ src/TKMaster.AulaCSharp.Core.WebApi/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace TKMaster.AulaCSharp.Core.WebApi
+{
+    public class Startup
+    {
+        #region Properties
+
+        public IConfiguration Configuration { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+        }
+               
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        } 
+        
+        #endregion
+    }
+}
